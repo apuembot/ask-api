ace76d3ac9228a79cf98bf444cc699033e713563
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-07-09 12:27:14 +0200

    Multiple routes

--- pages/api/[test]/[id].js
+++ pages/api/[test]/[id].js
@@ -1,11 +1,11 @@
-import { people } from '../../data'
+import { people } from '../../../data'
 
-export default function personHandler({ query: { id } }, res) {
+export default function personHandler({ query: { id, test }, }, res) {
   const filtered = people.filter((p) => p.id === id)
 
   // User with id exists
   if (filtered.length > 0) {
-    res.status(200).json(filtered[0])
+    res.status(200).json({ message: `User id: ${id}. Test: ${test} not found.` })
   } else {
     res.status(404).json({ message: `User with id: ${id} not found.` })
   }
