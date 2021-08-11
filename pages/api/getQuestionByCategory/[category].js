566fc841c1af7ef75e9114511599601ca5e48d0c
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-08-11 17:19:58 +0200

    Adding else

--- pages/api/getQuestionByCategory/[category].js
+++ pages/api/getQuestionByCategory/[category].js
@@ -5,15 +5,16 @@ export default function questionHandler({ query: { category }, }, res) {
   if (category === "general") {
     var randomQuestionId = Math.floor((Math.random() * questions.length) + 0);
     res.status(200).json(questions[randomQuestionId])
-  }
-
-  const filtered = questions.filter((p) => p.category === category)
-
-  // User with id exists
-  if (filtered.length > 0) {
-    var randomQuestionId = Math.floor((Math.random() * filtered.length) + 0);
-    res.status(200).json(filtered[randomQuestionId])
   } else {
-    res.status(404).json({ message: `Category with name: ${category} not found.` })
+
+    const filtered = questions.filter((p) => p.category === category)
+
+    // User with id exists
+    if (filtered.length > 0) {
+      var randomQuestionId = Math.floor((Math.random() * filtered.length) + 0);
+      res.status(200).json(filtered[randomQuestionId])
+    } else {
+      res.status(404).json({ message: `Category with name: ${category} not found.` })
+    }
   }
 }
