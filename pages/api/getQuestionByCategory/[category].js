75b767b5d54512e5be545a6b6bd1966d2e0c3e4a
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-08-14 17:29:59 +0200

    Prettier

--- pages/api/getQuestionByCategory/[category].js
+++ pages/api/getQuestionByCategory/[category].js
@@ -1,20 +1,20 @@
-import { questions } from '../../../data'
+import { questions } from "../../../data";
 
-export default function questionHandler({ query: { category }, }, res) {
-
+export default function questionHandler({ query: { category } }, res) {
   if (category === "general") {
-    var randomQuestionId = Math.floor((Math.random() * questions.length) + 0);
-    res.status(200).json(questions[randomQuestionId])
+    var randomQuestionId = Math.floor(Math.random() * questions.length + 0);
+    res.status(200).json(questions[randomQuestionId]);
   } else {
-
-    const filtered = questions.filter((p) => p.category === category)
+    const filtered = questions.filter((p) => p.category === category);
 
     // User with id exists
     if (filtered.length > 0) {
-      var randomQuestionId = Math.floor((Math.random() * filtered.length) + 0);
-      res.status(200).json(filtered[randomQuestionId])
+      var randomQuestionId = Math.floor(Math.random() * filtered.length + 0);
+      res.status(200).json(filtered[randomQuestionId]);
     } else {
-      res.status(404).json({ message: `Category with name: ${category} not found.` })
+      res
+        .status(404)
+        .json({ message: `Category with name: ${category} not found.` });
     }
   }
 }
